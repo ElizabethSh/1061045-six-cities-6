c7e7601fbea49cfbbdd2be3d7b6b9b0e679ff9aa
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-06 10:09:04 +0100

    Describe all routes in the app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import AuthPage from '../auth-page/auth-page';
+import Favorites from '../favorites/favorites';
+import Place from '../place/place';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 const App = (props) => {
   const {places} = props;
   return (
-    <MainPage
-      places = {places}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage places = {places}/>
+        </Route>
+        <Route path="/login" exact>
+          <AuthPage/>
+        </Route>
+        <Route path="/favorites" exact>
+          <Favorites />
+        </Route>
+        <Route path="/offer/:id" exact>
+          <Place />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
