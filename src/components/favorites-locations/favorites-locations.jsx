f76e5c92d1d28bef6ffc0de7bad69f561ac688b0
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-13 20:03:48 +0100

    Add the FavoriteLocations component

---
+++ src/components/favorites-locations/favorites-locations.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {CardName} from '../../common/const';
+import PlaceCard from '../place-card/place-card';
+import {placePropTypes} from '../../common/place-prop-types';
+
+const FavoriteLocations = (props) => {
+  const {places, city, onMouseEnter} = props;
+  return (
+    <li className="favorites__locations-items">
+      <div className="favorites__locations locations locations--current">
+        <div className="locations__item">
+          <a className="locations__item-link" href="#">
+            <span>{city}</span>
+          </a>
+        </div>
+      </div>
+      <div className="favorites__places">
+        {places.map((place) => {
+          return (
+            <PlaceCard key={place.id}
+              place={place}
+              cardName={CardName.FAVORITES}
+              onMouseEnter={onMouseEnter}
+            />
+          );
+        })}
+      </div>
+    </li>
+  );
+};
+
+FavoriteLocations.propTypes = {
+  places: PropTypes.arrayOf(
+      PropTypes.shape(placePropTypes)
+  ).isRequired,
+  city: PropTypes.string.isRequired,
+  onMouseEnter: PropTypes.func.isRequired,
+};
+
+export default FavoriteLocations;
