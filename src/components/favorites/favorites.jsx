51dd41529ee9ff0277c0ec9274a19566728509ef
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-14 20:26:50 +0100

    Remove state and event handler

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -1,18 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import PageHeader from '../page-header/page-header';
 import {placePropTypes} from '../../common/place-prop-types';
 import FavoriteLocations from '../favorites-locations/favorites-locations';
 
 const Favorites = (props) => {
-  const [activePlaceId, setActivePlace] = useState(0); // ВРЕМЕННО! Из-за задания 4!
-
   const {places} = props; // придут только избранные размещения!
-
-  // ПОВТОР обработчика из PlaceList!!! Из-за задания 4!
-  const handleCardMouseEnter = (place) => {
-    setActivePlace(place.id);
-  };
 
   // определение списка городов по которым нужно вывести избранные предложения
   const cities = [];
@@ -27,21 +20,16 @@ const Favorites = (props) => {
         <div className="page__favorites-container container">
           <section className="favorites">
             <h1 className="favorites__title">Saved listing</h1>
-
-            <div>{activePlaceId}</div> {/* ВРЕМЕННО!!!, чтобы линтер не ругался на неиспользование activePlace*/}
-
             <ul className="favorites__list">
-              {favoriteCities.map((city, index) => {
 
+              {favoriteCities.map((city, index) => {
                 // фильтрация избранных размещений по городу
                 const favoritePlaces = places.filter((place) => place.city.name === city);
-
                 return (
                   <FavoriteLocations
                     key={city + index}
                     places={favoritePlaces}
                     city={city}
-                    onMouseEnter={handleCardMouseEnter}
                   />
                 );
               })}
