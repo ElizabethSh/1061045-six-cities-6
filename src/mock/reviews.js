72327bc704278fa2cb30c9eccc87164a113b96bf
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-19 14:49:10 +0100

    Add reviews mocks

---
+++ src/mock/reviews.js
@@ -0,0 +1,44 @@
+export const reviews = [
+  {
+    offerId: 1,
+    comment: `A quiet cozy and picturesque that hides behind a
+      river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`,
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: `img/avatar-max.jpg`,
+      id: 4,
+      isPro: false, // зачем нужен этот флаг?
+      name: `Max`
+    }
+  },
+  {
+    offerId: 1,
+    comment: `A quiet cozy and picturesque that hides behind a
+      river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`,
+    id: 2,
+    rating: 5,
+    user: {
+      avatarUrl: `img/avatar-max.jpg`,
+      id: 5,
+      isPro: true, // зачем нужен этот флаг?
+      name: `Alex`
+    }
+  },
+  {
+    offerId: 2,
+    comment: `A quiet cozy and picturesque that hides behind a
+      river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`,
+    id: 3,
+    rating: 3,
+    user: {
+      avatarUrl: `img/avatar-max.jpg`,
+      id: 6,
+      isPro: true, // зачем нужен этот флаг?
+      name: `Frederik`
+    }
+  }
+];
