9852c889e7afbcb2078c9e247708f025220945cb
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-22 11:42:41 +0100

    Add array of cities

--- src/common/const.js
+++ src/common/const.js
@@ -8,3 +8,12 @@ export const CardsListName = {
   NEAR_PLACES_LIST: `near-places__list`,
   CITIES_PLACES_LIST: `cities__places-list tabs__content`
 };
+
+export const CITIES = [
+  `Paris`,
+  `Cologne`,
+  `Brussels`,
+  `Amsterdam`,
+  `Hamburg`,
+  `Dusseldorf`
+];
