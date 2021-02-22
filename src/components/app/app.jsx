580902b2f445544f1a63b2db5ecd95a33ed27b98
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-22 11:41:28 +0100

    Add the route for cities

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MainPage from '../main-page/main-page';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import MainPage from '../main-page/main-page';
 import AuthPage from '../auth-page/auth-page';
 import Favorites from '../favorites/favorites';
 import Place from '../place/place';
 import NotFoundPage from '../not-found-page/not-found-page';
-import {placeProp} from '../../common/prop-types/place.prop';
 import {reviewProp} from '../../common/prop-types/review.prop';
 
 const App = (props) => {
-  const {places, reviews} = props;
-
-  // сортировка только по флагу isFavorite
-  const favoritePlaces = places.filter((place) => place.isFavorite === true);
+  const {reviews} = props;
 
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact>
-          <MainPage places = {places}/>
+          <MainPage/>
         </Route>
         <Route path="/login" exact>
           <AuthPage/>
         </Route>
         <Route path="/favorites" exact>
-          <Favorites places = {favoritePlaces}/>
+          <Favorites/>
         </Route>
+
+        <Route path="/city/:city" >
+          <MainPage/>
+        </Route>
+
         <Route path="/offer/:id">
           <Place
-            places={places}
             reviews={reviews}
           />
         </Route>
@@ -42,9 +42,6 @@ const App = (props) => {
 };
 
 App.propTypes = {
-  places: PropTypes.arrayOf(
-      PropTypes.shape(placeProp)
-  ).isRequired,
   reviews: PropTypes.arrayOf(
       PropTypes.shape(reviewProp)
   ).isRequired,
