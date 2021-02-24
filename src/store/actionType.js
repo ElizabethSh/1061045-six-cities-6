a12e8a4f79b043ccdd8ebf07ed914ecf822833f7
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-24 20:36:33 +0100

    Add new action types

--- src/store/actionType.js
+++ src/store/actionType.js
@@ -1,4 +1,11 @@
 export const ActionType = {
   CITY_CHANGE: `city/change`,
-  CITY_RESET: `city/reset`
+  PLACE_LIST_CHANGE: `city/place-list-change`,
+  CITY_RESET: `city/reset`,
+
+  CARD_HOVER: `city/cardHover`,
+  CARD_HOVER_RESET: `city/cardHoverReset`,
+
+  SORT_TYPE_CHANGE: `city/sort-type-change`,
 };
+
