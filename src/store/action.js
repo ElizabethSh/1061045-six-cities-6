b4163ef2085786f64877015aa0b8be233be43b65
ElizabethSh <liza.shoshkina@gmail.com>
2021-02-25 16:03:02 +0100

    Add sortPlacesListAction

--- src/store/action.js
+++ src/store/action.js
@@ -1,4 +1,6 @@
-import {ActionType} from "./actionType";
+import {SortType} from "../common/const";
+import {sortOffersByRating, sortOffersHightToLowPrice, sortOffersLowToHightPrice} from "../common/sort";
+import {ActionType, SORT_PLACES_LIST} from "./actionType";
 
 export const ActionCreator = {
   cityChangeAction: (activeCity) => ({
@@ -28,4 +30,33 @@ export const ActionCreator = {
     type: ActionType.SORT_TYPE_CHANGE,
     payload: sortType
   }),
+
+  // -----------------------------------------------
+
+  sortPlacesListAction: (sortType, activeCityPlaces) => {
+    let sortedList = activeCityPlaces.slice();
+
+    switch (sortType) {
+
+      case SortType.TOP_RATED:
+        sortOffersByRating(sortedList);
+        break;
+
+      case SortType.PRICE_HIGHT_TO_LOW:
+        sortOffersHightToLowPrice(sortedList);
+        break;
+
+      case SortType.PRICE_LOW_TO_HIGHT:
+        sortOffersLowToHightPrice(sortedList);
+        break;
+
+      default:
+        break;
+    }
+
+    return ({
+      type: SORT_PLACES_LIST,
+      payload: sortedList
+    });
+  }
 };
