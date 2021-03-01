8abafb9868cf9e59d2ee96e2cb35955e312c6b40
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-01 16:36:25 +0100

    Connect thunk to the app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {reducer} from './store/reducer';
 import {reviews} from './mock/reviews';
+import {createAPI} from './services/api';
+import {ActionCreator} from './store/action';
+import {AuthStatus} from './common/const';
 
 const rootReducer = combineReducers({reducer});
 
+const api = createAPI(() => store.dispatch(ActionCreator.setAuthStatusAction(AuthStatus.NO_AUTH)));
+
 const store = createStore(
     rootReducer,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 ReactDOM.render(
