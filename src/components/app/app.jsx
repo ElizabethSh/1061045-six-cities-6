f0cc516c85db9a63893129d502b08ddd183378f0
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-01 16:31:15 +0100

    Add data loading

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
 import MainPage from '../main-page/main-page';
 import AuthPage from '../auth-page/auth-page';
 import Favorites from '../favorites/favorites';
 import Place from '../place/place';
+import Loader from '../loader/loader';
 import NotFoundPage from '../not-found-page/not-found-page';
 import {reviewProp} from '../../common/prop-types/review.prop';
+import {fetchOffersList} from '../../store/api-actions';
 
 const App = (props) => {
-  const {reviews} = props;
+  const {reviews, isDataLoaded, onDataLoad} = props;
+
+  useEffect(() => {
+    if (!isDataLoaded) {
+      onDataLoad();
+    }
+  }, [isDataLoaded]);
+
+  if (!isDataLoaded) {
+    return (
+      <Loader />
+    );
+  }
 
   return (
     <BrowserRouter>
@@ -41,10 +56,24 @@ const App = (props) => {
   );
 };
 
+const mapStateToProps = (state) => {
+  return {
+    isDataLoaded: state.reducer.isDataLoaded,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onDataLoad: () => dispatch(fetchOffersList()),
+  };
+};
+
 App.propTypes = {
   reviews: PropTypes.arrayOf(
       PropTypes.shape(reviewProp)
   ).isRequired,
+  isDataLoaded: PropTypes.bool.isRequired,
+  onDataLoad: PropTypes.func.isRequired,
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
