de82ebe349e8de915015ed15c11d47eed7f3c8d3
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-04 15:19:50 +0100

    Add the Private Route component

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Redirect, Route} from 'react-router-dom';
+import {AppRoute} from '../../common/const';
+
+const PrivateRoute = (props) => {
+  const {path, exact, isLoggedIn, render} = props;
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+    >
+      {isLoggedIn ? render() : <Redirect to={AppRoute.LOGIN} />}
+    </Route>
+  );
+};
+
+PrivateRoute.propTypes = {
+  path: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.reducer.isLoggedIn
+  };
+};
+
+export default connect(mapStateToProps)(PrivateRoute);
