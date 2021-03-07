937adfa141c459a55e9114825eab2d38239e498b
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-07 20:56:04 +0100

    Remove the adaptData function

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,33 +1,10 @@
 import {APIRoute} from "../common/const";
+import {adaptOffersData} from "../services/adapter";
 import {ActionCreator} from "./action";
-
-const adaptData = (data) => {
-  const adaptedData = ({
-    ...data,
-    isFavorite: data.is_favorite,
-    isPremium: data.is_premium,
-    previewImage: data.preview_image,
-    maxAdults: data.max_adults,
-    host: {
-      ...data.host,
-      avatarUrl: data.host.avatar_url,
-      isPro: data.host.is_pro
-    }
-  });
-
-  delete adaptedData.is_favorite;
-  delete adaptedData.is_premium;
-  delete adaptedData.preview_image;
-  delete adaptedData.max_adults;
-  delete adaptedData.host.avatar_url;
-  delete adaptedData.host.is_pro;
-
-  return adaptedData;
-};
 
 export const fetchOffersList = () => (dispatch, _getState, api) => {
   api.get(APIRoute.HOTELS)
-    .then(({data}) => data.map((it) => adaptData(it)))
+    .then(({data}) => data.map((it) => adaptOffersData(it)))
     .then((data) => dispatch(ActionCreator.loadDataAction(data)));
 };
 
