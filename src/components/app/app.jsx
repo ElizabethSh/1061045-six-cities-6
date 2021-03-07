279017ea52e74831ba3f9e398a18ecd85b288603
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-07 21:39:34 +0100

    Remove offers loading, add the error route

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,33 +1,14 @@
-import React, {useEffect} from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import {connect} from 'react-redux';
 import MainPage from '../main-page/main-page';
 import AuthPage from '../auth-page/auth-page';
 import Favorites from '../favorites/favorites';
 import Place from '../place/place';
-import Loader from '../loader/loader';
 import NotFoundPage from '../not-found-page/not-found-page';
-import {reviewProp} from '../../common/prop-types/review.prop';
-import {fetchOffersList} from '../../store/api-actions';
 import PrivateRoute from '../private-route/private-route';
 import {AppRoute} from '../../common/const';
 
-const App = (props) => {
-  const {reviews, isDataLoaded, onDataLoad} = props;
-
-  useEffect(() => {
-    if (!isDataLoaded) {
-      onDataLoad();
-    }
-  }, [isDataLoaded]);
-
-  if (!isDataLoaded) {
-    return (
-      <Loader />
-    );
-  }
-
+const App = () => {
   return (
     <BrowserRouter>
       <Switch>
@@ -49,9 +30,10 @@ const App = (props) => {
         </Route>
 
         <Route path={AppRoute.OFFER}>
-          <Place
-            reviews={reviews}
-          />
+          <Place/>
+        </Route>
+        <Route path={AppRoute.ERROR}>
+          <NotFoundPage />
         </Route>
         <Route>
           <NotFoundPage />
@@ -61,24 +43,4 @@ const App = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    isDataLoaded: state.reducer.isDataLoaded,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onDataLoad: () => dispatch(fetchOffersList()),
-  };
-};
-
-App.propTypes = {
-  reviews: PropTypes.arrayOf(
-      PropTypes.shape(reviewProp)
-  ).isRequired,
-  isDataLoaded: PropTypes.bool.isRequired,
-  onDataLoad: PropTypes.func.isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
