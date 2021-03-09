ae974209c7048e4be66bae4e347b2437a18db6c9
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-09 22:06:40 +0100

    Add the functions: fetchPlace, fetchNearPlaces

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,6 +1,7 @@
 import {APIRoute} from "../common/const";
 import {adaptOffersData, adaptReviewsData} from "../services/adapter";
 import {ActionCreator} from "./action";
+import {api as loadApi} from "../index";
 
 export const fetchOffersList = () => (dispatch, _getState, api) => {
   api.get(APIRoute.HOTELS)
@@ -12,7 +13,8 @@ export const checkAuth = () => (dispatch, _getState, api) => {
   api.get(APIRoute.LOGIN)
     .then(({data}) => dispatch(ActionCreator.setUsersEmailAction(data.email)))
     .then(() => dispatch(ActionCreator.setAuthStatusAction(true)))
-    .catch(() => {});
+    .then(() => dispatch(ActionCreator.checkAuthAction()))
+    .catch(() => dispatch(ActionCreator.checkAuthAction()));
 };
 
 export const logIn = ({email, password}) => (dispatch, _getState, api) => {
@@ -27,6 +29,16 @@ export const logOut = () => (dispatch, _getState, api) => {
     .then(() => dispatch(ActionCreator.setAuthStatusAction(false)))
     .then(() => dispatch(ActionCreator.setUsersEmailAction(null)))
     .catch(() => {});
+};
+
+export const fetchPlace = (id) => {
+  return loadApi.get(`hotels/${id}`)
+    .then(({data}) => adaptOffersData(data));
+};
+
+export const fetchNearPlaces = (id) => {
+  return loadApi.get(`/hotels/${id}/nearby`)
+  .then(({data}) => data.map((it) => adaptOffersData(it)));
 };
 
 export const fetchPlaceReviews = (placeId) => (dispatch, _getState, api) => {
