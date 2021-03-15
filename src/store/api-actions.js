515931c950b94f2b3fd5fa3ee54f84bda13c453f
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-15 15:51:09 +0100

    Add the functions: addToFavorite, fetchFavoritePlaces

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,34 +1,42 @@
 import {APIRoute} from "../common/const";
 import {adaptOffersData, adaptReviewsData} from "../services/adapter";
-import {ActionCreator} from "./action";
+import {loadReviews} from "./reducer/reviews/reviews-action";
 import {api as loadApi} from "../index";
+import {checkAuthAttempt, setAuthStatus, setUsersEmail} from "./reducer/user/user-action";
+import {changeFavoriteStatus, loadOffers} from "./reducer/offers/offers-action";
 
 export const fetchOffersList = () => (dispatch, _getState, api) => {
   api.get(APIRoute.HOTELS)
     .then(({data}) => data.map((it) => adaptOffersData(it)))
-    .then((data) => dispatch(ActionCreator.loadDataAction(data)));
+    .then((data) => dispatch(loadOffers(data)));
 };
 
 export const checkAuth = () => (dispatch, _getState, api) => {
   api.get(APIRoute.LOGIN)
-    .then(({data}) => dispatch(ActionCreator.setUsersEmailAction(data.email)))
-    .then(() => dispatch(ActionCreator.setAuthStatusAction(true)))
-    .then(() => dispatch(ActionCreator.checkAuthAction()))
-    .catch(() => dispatch(ActionCreator.checkAuthAction()));
+    .then(({data}) => dispatch(setUsersEmail(data.email)))
+    .then(() => dispatch(setAuthStatus(true)))
+    .then(() => dispatch(checkAuthAttempt()))
+    .catch(() => dispatch(checkAuthAttempt()));
 };
 
 export const logIn = ({email, password}) => (dispatch, _getState, api) => {
   api.post(APIRoute.LOGIN, {email, password})
-    .then(() => dispatch(ActionCreator.setAuthStatusAction(true)))
-    .then(() => dispatch(ActionCreator.setUsersEmailAction(email)))
+    .then(() => dispatch(setAuthStatus(true)))
+    .then(() => dispatch(setUsersEmail(email)))
     .catch(() => {});
 };
 
 export const logOut = () => (dispatch, _getState, api) => {
   api.get(APIRoute.LOGOUT)
-    .then(() => dispatch(ActionCreator.setAuthStatusAction(false)))
-    .then(() => dispatch(ActionCreator.setUsersEmailAction(null)))
+    .then(() => dispatch(setAuthStatus(false)))
+    .then(() => dispatch(setUsersEmail(null)))
     .catch(() => {});
+};
+
+export const addToFavorite = (id, status) => (dispatch, _getState, api) => {
+  api.post(`/favorite/${id}/${status}`)
+    .then(({data}) => adaptOffersData(data))
+    .then((data) => dispatch(changeFavoriteStatus(data)));
 };
 
 export const fetchPlace = (id) => {
@@ -41,16 +49,21 @@ export const fetchNearPlaces = (id) => {
   .then(({data}) => data.map((it) => adaptOffersData(it)));
 };
 
+export const fetchFavoritePlaces = () => {
+  return loadApi.get(`/favorite`)
+    .then(({data}) => data.map((it) => adaptOffersData(it)));
+};
+
 export const fetchPlaceReviews = (placeId) => (dispatch, _getState, api) => {
   api.get(`comments/${placeId}`)
     .then(({data}) => data.map((it) => adaptReviewsData(it)))
-    .then((data) => dispatch(ActionCreator.loadReviewsAction(data)))
+    .then((data) => dispatch(loadReviews(data)))
     .catch(() => {});
 };
 
 export const sendPlaceReview = (id, {rating, comment}) => (dispatch, _getState, api) => {
   api.post(`comments/${id}`, {rating, comment})
   .then(({data}) => data.map((it) => adaptReviewsData(it)))
-  .then((data) => dispatch(ActionCreator.loadReviewsAction(data)))
+  .then((data) => dispatch(loadReviews(data)))
   .catch(() => {});
 };
