b64cbafb4501a0ddbb4029edf98f2fececcd805b
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-15 15:10:58 +0100

    Rename file actionType.js to action-type.js

---
+++ src/store/action-type.js
@@ -0,0 +1,21 @@
+export const ActionType = {
+  LOAD_OFFERS: `offers/loadOffers`,
+  CITY_CHANGE: `offers/changeCity`,
+  CHANGE_PLACE_LIST: `offers/changePlaceList`,
+  CITY_RESET: `offers/resetCity`,
+
+  CARD_HOVER: `offers/cardHover`,
+  RESET_CARD_HOVER: `offers/resetCardHover`,
+
+  CHANGE_SORT_TYPE: `sort/changeSortType`,
+  SORT_PLACES_LIST: `sort/sortPlacesList`,
+
+  LOAD_REVIEWS: `review/loadReviews`,
+  RESET_REVIEWS: `review/resetReviews`,
+
+  REQUIRED_AUTH: `user/requiredAuth`,
+  CHECK_AUTH: `user/checkAuth`,
+  SET_USERS_EMAIL: `user/setUsersEmail`,
+
+  CHANGE_FAVORITE_STATUS: `offer/changeFavoriteStatus`
+};
