8d9ec955daba0a2b69453b8f7fac1f452a274a01
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-17 19:48:49 +0100

    Add the placeInfo reducer

--- src/store/reducer/root-reducer.js
+++ src/store/reducer/root-reducer.js
@@ -3,17 +3,20 @@ import {offers} from './offers/offers';
 import {user} from "./user/user";
 import {reviews} from "./reviews/reviews";
 import {sort} from "./sort/sort";
+import {placeInfo} from "./place-info/place-info";
 
 export const NameSpace = {
   USER: `USER`,
   REVIEW: `REVIEW`,
   SORT: `SORT`,
-  OFFER: `OFFER`
+  OFFER: `OFFER`,
+  PLACE_INFO: `PLACE_INFO`
 };
 
 export const rootReducer = combineReducers({
   [NameSpace.USER]: user,
   [NameSpace.REVIEW]: reviews,
   [NameSpace.SORT]: sort,
-  [NameSpace.OFFER]: offers
+  [NameSpace.OFFER]: offers,
+  [NameSpace.PLACE_INFO]: placeInfo
 });
