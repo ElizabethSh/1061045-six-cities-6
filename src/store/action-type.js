f81d5c12efc942cbfd46d0cbbc5e6f321e6c97b0
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-17 19:11:59 +0100

    Add the action types: LOAD_PLACE_INFO, RESET_PLACE_INFO

--- src/store/action-type.js
+++ src/store/action-type.js
@@ -1,14 +1,12 @@
 export const ActionType = {
   LOAD_OFFERS: `offers/loadOffers`,
   CITY_CHANGE: `offers/changeCity`,
-  CHANGE_PLACE_LIST: `offers/changePlaceList`,
   CITY_RESET: `offers/resetCity`,
-
   CARD_HOVER: `offers/cardHover`,
   RESET_CARD_HOVER: `offers/resetCardHover`,
+  CHANGE_FAVORITE_STATUS: `offers/changeFavoriteStatus`,
 
   CHANGE_SORT_TYPE: `sort/changeSortType`,
-  SORT_PLACES_LIST: `sort/sortPlacesList`,
 
   LOAD_REVIEWS: `review/loadReviews`,
   RESET_REVIEWS: `review/resetReviews`,
@@ -17,5 +15,6 @@ export const ActionType = {
   CHECK_AUTH: `user/checkAuth`,
   SET_USERS_EMAIL: `user/setUsersEmail`,
 
-  CHANGE_FAVORITE_STATUS: `offer/changeFavoriteStatus`
+  LOAD_PLACE_INFO: `place/loadPlaceInfo`,
+  RESET_PLACE_INFO: `place/resetPlaceInfo`
 };
