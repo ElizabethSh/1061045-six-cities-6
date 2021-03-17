8cc8babb1c01b40c65eac619682572cd49aab8bc
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-17 19:49:45 +0100

    Delete the sortPlacesList action

--- src/store/reducer/sort/sort-action.js
+++ src/store/reducer/sort/sort-action.js
@@ -1,39 +1,6 @@
-import {SortType} from "../../../common/const";
 import {ActionType} from "../../action-type";
-import {
-  sortOffersByRating,
-  sortOffersHightToLowPrice,
-  sortOffersLowToHightPrice
-} from "../../../common/sort";
 
 export const setSortType = (sortType) => ({
   type: ActionType.CHANGE_SORT_TYPE,
   payload: sortType
 });
-
-export const sortPlacesList = (activeCityPlaces, sortType) => {
-  let sortedList = activeCityPlaces.slice();
-
-  switch (sortType) {
-
-    case SortType.TOP_RATED:
-      sortOffersByRating(sortedList);
-      break;
-
-    case SortType.PRICE_HIGHT_TO_LOW:
-      sortOffersHightToLowPrice(sortedList);
-      break;
-
-    case SortType.PRICE_LOW_TO_HIGHT:
-      sortOffersLowToHightPrice(sortedList);
-      break;
-
-    default:
-      break;
-  }
-
-  return ({
-    type: ActionType.SORT_PLACES_LIST,
-    payload: sortedList
-  });
-};
