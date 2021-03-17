99e66ec7ace5284ecefc345c37220f8fed6d6e73
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-17 18:57:46 +0100

    Add selectors to the offers reducer

---
+++ src/store/reducer/offers/selectors.js
@@ -0,0 +1,14 @@
+import {createSelector} from "reselect";
+import {getCityPlaces} from "../../../common/utils";
+import {NameSpace} from "../root-reducer";
+
+export const getOffers = (state) => state[NameSpace.OFFER].offers;
+export const getActiveCity = (state) => state[NameSpace.OFFER].activeCity;
+
+export const getIsOffersLoaded = (state) => state[NameSpace.OFFER].isOffersLoaded;
+export const getActiveCard = (state) => state[NameSpace.OFFER].activeCard;
+
+export const getActiveCityPlaces = createSelector(
+    [getOffers, getActiveCity],
+    (offers, activeCity) => getCityPlaces(offers, activeCity)
+);
