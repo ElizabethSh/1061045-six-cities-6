3917f7ccb4e8065cbe241d554f8a9ff18c1c37ef
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-17 19:48:03 +0100

    Delete the cases CHANGE_PLACE_LIST

--- src/store/reducer/offers/offers.js
+++ src/store/reducer/offers/offers.js
@@ -1,26 +1,26 @@
 import {ActionType} from '../../action-type';
-import {getCityPlaces, updateOffers} from '../../../common/utils';
+import {updateOffers} from '../../../common/utils';
 
 const initialState = {
   activeCity: `Paris`,
   offers: [],
   isOffersLoaded: false,
   activeCard: 0,
-  activeCityPlaces: [],
 };
 
 export const offers = (state = initialState, action) => {
   switch (action.type) {
+    case ActionType.LOAD_OFFERS:
+      return ({
+        ...state,
+        offers: action.payload,
+        isOffersLoaded: true
+      });
+
     case ActionType.CITY_CHANGE:
       return ({
         ...state,
         activeCity: action.payload
-      });
-
-    case ActionType.CHANGE_PLACE_LIST:
-      return ({
-        ...state,
-        activeCityPlaces: getCityPlaces(state.offers, state.activeCity)
       });
 
     case ActionType.CITY_RESET:
@@ -39,13 +39,6 @@ export const offers = (state = initialState, action) => {
       return ({
         ...state,
         activeCard: 0,
-      });
-
-    case ActionType.LOAD_OFFERS:
-      return ({
-        ...state,
-        offers: action.payload,
-        isOffersLoaded: true
       });
 
     case ActionType.CHANGE_FAVORITE_STATUS:
