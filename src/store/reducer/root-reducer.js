58cb9a053a0f1e1ff73b359a35a3a806c4212e27
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-18 12:25:51 +0100

    Add the favorites reducer

--- src/store/reducer/root-reducer.js
+++ src/store/reducer/root-reducer.js
@@ -4,13 +4,15 @@ import {user} from "./user/user";
 import {reviews} from "./reviews/reviews";
 import {sort} from "./sort/sort";
 import {placeInfo} from "./place-info/place-info";
+import {favorites} from "./favorites/favorites";
 
 export const NameSpace = {
   USER: `USER`,
   REVIEW: `REVIEW`,
   SORT: `SORT`,
   OFFER: `OFFER`,
-  PLACE_INFO: `PLACE_INFO`
+  PLACE_INFO: `PLACE_INFO`,
+  FAVORITE: `FAVORITE`
 };
 
 export const rootReducer = combineReducers({
@@ -18,5 +20,6 @@ export const rootReducer = combineReducers({
   [NameSpace.REVIEW]: reviews,
   [NameSpace.SORT]: sort,
   [NameSpace.OFFER]: offers,
-  [NameSpace.PLACE_INFO]: placeInfo
+  [NameSpace.PLACE_INFO]: placeInfo,
+  [NameSpace.FAVORITE]: favorites,
 });
