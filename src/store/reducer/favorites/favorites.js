78385a56e43f1af4324d847aecd1de97af9fd66f
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-18 12:23:37 +0100

    Add directory reducer/favorites/

---
+++ src/store/reducer/favorites/favorites.js
@@ -0,0 +1,26 @@
+import {ActionType} from "../../action-type";
+
+const initialState = {
+  favorites: [],
+  isFavoritesLoaded: false,
+};
+
+export const favorites = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.LOAD_FAVORITES:
+      return ({
+        ...state,
+        favorites: action.payload,
+        isFavoritesLoaded: true
+      });
+
+    case ActionType.RESET_FAVORITES:
+      return ({
+        ...state,
+        isFavoritesLoaded: false
+      });
+
+    default:
+      return state;
+  }
+};
