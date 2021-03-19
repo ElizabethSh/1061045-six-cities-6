854544d153060e3df6518d0263e96a51f8fe37d2
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-19 16:46:46 +0100

    Add the reducer/near-places/ directory

---
+++ src/store/reducer/near-places/near-places.js
@@ -0,0 +1,26 @@
+import {ActionType} from "../../action-type";
+
+const initialState = {
+  nearPlaces: [],
+  isNearPlacesLoaded: false
+};
+
+export const nearPlaces = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.LOAD_NEAR_PLACES:
+      return ({
+        ...state,
+        nearPlaces: action.payload,
+        isNearPlacesLoaded: true
+      });
+
+    case ActionType.RESET_NEAR_PLACES:
+      return ({
+        ...state,
+        isNearPlacesLoaded: false
+      });
+
+    default:
+      return state;
+  }
+};
