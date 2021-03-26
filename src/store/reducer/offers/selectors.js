b4dbe6c03d2e10228f9fa038277ff501db74698d
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-26 20:01:43 +0100

    Delete the getActiveCard selector

--- src/store/reducer/offers/selectors.js
+++ src/store/reducer/offers/selectors.js
@@ -6,7 +6,6 @@ export const getOffers = (state) => state[NameSpace.OFFER].offers;
 export const getActiveCity = (state) => state[NameSpace.OFFER].activeCity;
 
 export const getIsOffersLoaded = (state) => state[NameSpace.OFFER].isOffersLoaded;
-export const getActiveCard = (state) => state[NameSpace.OFFER].activeCard;
 export const getIsErrorStatus = (state) => state[NameSpace.OFFER].isError;
 
 export const getActiveCityPlaces = createSelector(
