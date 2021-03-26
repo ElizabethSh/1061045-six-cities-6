7c11e52e281728b2d3bdec99aa2be77a309ed6c6
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-26 20:53:24 +0100

    Refactoring

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,21 +1,21 @@
 import {APIRoute} from "../common/const";
 import {adaptOffersData, adaptReviewsData} from "../services/adapter";
-import {loadReviews} from "./reducer/reviews/reviews-action";
-import {checkAuthAttempt, setAuthStatus, setUsersEmail} from "./reducer/user/user-action";
-import {changeErrorStatus, loadOffers} from "./reducer/offers/offers-action";
-import {loadPlaceInfo} from "./reducer/place-info/place-info-action";
-import {loadFavorites} from "./reducer/favorites/favorites-action";
-import {loadNearPlaces} from "./reducer/near-places/near-places-action";
+import {loadReviews} from "./reducer/reviews/action";
+import {checkAuthAttempt, setAuthStatus, setUsersEmail} from "./reducer/user/action";
+import {changeErrorStatus, loadOffers} from "./reducer/offers/action";
+import {loadPlaceInfo} from "./reducer/place-info/action";
+import {loadFavorites} from "./reducer/favorites/action";
+import {loadNearPlaces} from "./reducer/near-places/action";
 
 export const fetchOffersList = () => (dispatch, _getState, api) => {
-  api.get(APIRoute.HOTELS)
+  return api.get(APIRoute.HOTELS)
     .then(({data}) => data.map((it) => adaptOffersData(it)))
     .then((data) => dispatch(loadOffers(data)))
     .catch(() => dispatch(changeErrorStatus(true)));
 };
 
 export const checkAuth = () => (dispatch, _getState, api) => {
-  api.get(APIRoute.LOGIN)
+  return api.get(APIRoute.LOGIN)
     .then(({data}) => dispatch(setUsersEmail(data.email)))
     .then(() => dispatch(setAuthStatus(true)))
     .then(() => dispatch(checkAuthAttempt()))
@@ -23,21 +23,42 @@ export const checkAuth = () => (dispatch, _getState, api) => {
 };
 
 export const logIn = ({email, password}) => (dispatch, _getState, api) => {
-  api.post(APIRoute.LOGIN, {email, password})
+  return api.post(APIRoute.LOGIN, {email, password})
     .then(() => dispatch(setAuthStatus(true)))
     .then(() => dispatch(setUsersEmail(email)))
     .catch(() => dispatch(changeErrorStatus(true)));
 };
 
 export const logOut = () => (dispatch, _getState, api) => {
-  api.get(APIRoute.LOGOUT)
+  return api.get(APIRoute.LOGOUT)
     .then(() => dispatch(setAuthStatus(false)))
     .then(() => dispatch(setUsersEmail(null)))
     .catch(() => dispatch(changeErrorStatus(true)));
 };
 
+export const fetchPlace = (id) => (dispatch, _getState, api) => {
+  return api.get(`${APIRoute.HOTELS}/${id}`)
+    .then(({data}) => adaptOffersData(data))
+    .then((data) => dispatch(loadPlaceInfo(data)))
+    .catch(() => dispatch(changeErrorStatus(true)));
+};
+
+export const fetchNearPlaces = (id) => (dispatch, _getState, api) => {
+  return api.get(`${APIRoute.HOTELS}/${id}/nearby`)
+    .then(({data}) => data.map((it) => adaptOffersData(it)))
+    .then((data) => dispatch(loadNearPlaces(data)))
+    .catch(() => dispatch(changeErrorStatus(true)));
+};
+
+export const fetchFavoritePlaces = () => (dispatch, _getState, api) => {
+  return api.get(APIRoute.FAVORITE)
+    .then(({data}) => data.map((it) => adaptOffersData(it)))
+    .then((data) => dispatch(loadFavorites(data)))
+    .catch(() => dispatch(changeErrorStatus(true)));
+};
+
 export const addToFavorite = (id, status) => (dispatch, _getState, api) => {
-  return api.post(`/favorite/${id}/${status}`)
+  return api.post(`${APIRoute.FAVORITE}/${id}/${status}`)
     .then(({data}) => adaptOffersData(data))
     .catch((error) => {
       dispatch(changeErrorStatus(true));
@@ -45,36 +66,15 @@ export const addToFavorite = (id, status) => (dispatch, _getState, api) => {
     });
 };
 
-export const fetchPlace = (id) => (dispatch, _getState, api) => {
-  return api.get(`hotels/${id}`)
-    .then(({data}) => adaptOffersData(data))
-    .then((data) => dispatch(loadPlaceInfo(data)))
-    .catch(() => dispatch(changeErrorStatus(true)));
-};
-
-export const fetchNearPlaces = (id) => (dispatch, _getState, api) => {
-  return api.get(`/hotels/${id}/nearby`)
-    .then(({data}) => data.map((it) => adaptOffersData(it)))
-    .then((data) => dispatch(loadNearPlaces(data)))
-    .catch(() => dispatch(changeErrorStatus(true)));
-};
-
-export const fetchFavoritePlaces = () => (dispatch, _getState, api) => {
-  return api.get(`/favorite`)
-    .then(({data}) => data.map((it) => adaptOffersData(it)))
-    .then((data) => dispatch(loadFavorites(data)))
-    .catch(() => dispatch(changeErrorStatus(true)));
-};
-
-export const fetchPlaceReviews = (placeId) => (dispatch, _getState, api) => {
-  api.get(`comments/${placeId}`)
+export const fetchPlaceReviews = (id) => (dispatch, _getState, api) => {
+  return api.get(`${APIRoute.COMMENTS}/${id}`)
     .then(({data}) => data.map((it) => adaptReviewsData(it)))
     .then((data) => dispatch(loadReviews(data)))
     .catch(() => dispatch(changeErrorStatus(true)));
 };
 
 export const sendPlaceReview = (id, {rating, comment}) => (dispatch, _getState, api) => {
-  return api.post(`comments/${id}`, {rating, comment})
+  return api.post(`${APIRoute.COMMENTS}/${id}`, {rating, comment})
     .then(({data}) => data.map((it) => adaptReviewsData(it)))
     .then((data) => dispatch(loadReviews(data)))
     .catch(() => dispatch(changeErrorStatus(true)));
