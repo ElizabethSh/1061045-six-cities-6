cf0efca0f35688b13c3bae20b4db28a656144b6b
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-27 21:14:02 +0100

    Delete selectors: getIsOffersLoaded, getIsErrorStatus

--- src/store/reducer/offers/selectors.js
+++ src/store/reducer/offers/selectors.js
@@ -5,9 +5,6 @@ import {NameSpace} from "../root-reducer";
 export const getOffers = (state) => state[NameSpace.OFFER].offers;
 export const getActiveCity = (state) => state[NameSpace.OFFER].activeCity;
 
-export const getIsOffersLoaded = (state) => state[NameSpace.OFFER].isOffersLoaded;
-export const getIsErrorStatus = (state) => state[NameSpace.OFFER].isError;
-
 export const getActiveCityPlaces = createSelector(
     [getOffers, getActiveCity],
     (offers, activeCity) => getCityPlaces(offers, activeCity)
