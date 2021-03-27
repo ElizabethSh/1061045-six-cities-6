ab65fb55e62b2d616da4c32cfd331e02db2e8fe5
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-27 20:59:49 +0100

    Replace connect with hooks

--- src/components/private-route/private-route.jsx
+++ src/components/private-route/private-route.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {Redirect, Route} from 'react-router-dom';
 import {AppRoute} from '../../common/const';
 
 const PrivateRoute = (props) => {
-  const {path, exact, isLoggedIn, render} = props;
+  const {path, exact, render} = props;
+  const {isLoggedIn} = useSelector((state) => state.USER);
 
   return (
     <Route
       path={path}
       exact={exact}
     >
-      {isLoggedIn ? render() : <Redirect to={AppRoute.LOGIN} />}
+      {
+        isLoggedIn
+          ? render()
+          : <Redirect to={AppRoute.LOGIN} />
+      }
     </Route>
   );
 };
@@ -20,14 +25,7 @@ const PrivateRoute = (props) => {
 PrivateRoute.propTypes = {
   path: PropTypes.string.isRequired,
   exact: PropTypes.bool.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
   render: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({USER}) => {
-  return {
-    isLoggedIn: USER.isLoggedIn
-  };
-};
-
-export default connect(mapStateToProps)(PrivateRoute);
+export default PrivateRoute;
