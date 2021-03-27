d796afaf25a1212b285d62910d6aba5f1b958ff3
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-27 20:52:14 +0100

    Delete selectors

--- src/store/reducer/near-places/selectors.js
+++
@@ -1,5 +0,0 @@
-import {NameSpace} from "../root-reducer";
-
-export const getNearPlaces = (state) => state[NameSpace.NEAR_PLACE].nearPlaces;
-
-export const getIsNearPlacesLoaded = (state) => state[NameSpace.NEAR_PLACE].isNearPlacesLoaded;
