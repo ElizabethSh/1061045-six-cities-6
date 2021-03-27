5f9c085cf544181fdf304a450b59ec84b9dd3fc8
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-27 21:05:09 +0100

    Add snapshot test for the Review component

---
+++ src/components/review/review.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {render} from "@testing-library/react";
+import Review from './review';
+
+it(`Review should render correctly`, () => {
+  const review = {
+    offerId: 2,
+    comment: `Cozy`,
+    date: `2019-12-08T14:13:56.569Z`,
+    id: 3,
+    rating: 3,
+    user: {
+      avatarUrl: `img/avatar-max.jpg`,
+      id: 6,
+      isPro: true,
+      name: `Frederik`
+    }
+  };
+
+  const {container} = render(
+      <Review review={review}/>
+  );
+
+  expect(container).toMatchSnapshot();
+});
