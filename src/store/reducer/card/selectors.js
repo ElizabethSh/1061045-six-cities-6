d92a5983552449603c915d50f6eca3e9aa15099b
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-27 20:51:56 +0100

    Delete selectors

--- src/store/reducer/card/selectors.js
+++
@@ -1,3 +0,0 @@
-import {NameSpace} from "../root-reducer";
-
-export const getActiveCard = (state) => state[NameSpace.CARD].activeCard;
--- src/store/reducer/favorites/selectors.js
+++
@@ -1,5 +0,0 @@
-import {NameSpace} from "../root-reducer";
-
-export const getFavoritePlaces = (state) => state[NameSpace.FAVORITE].favorites;
-
-export const getIsFavoritesLoaded = (state) => state[NameSpace.FAVORITE].isFavoritesLoaded;
