9c406765d201b9f50dbcff87fbf966656c603725
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-29 12:49:28 +0200

    Add snapshot test

--- src/components/empty-favorites-container/empty-favorites-container.test.js
+++ src/components/empty-favorites-container/empty-favorites-container.test.js
@@ -3,15 +3,9 @@ import {render} from '@testing-library/react';
 import EmptyFavoritesContainer from './empty-favorites-container';
 
 it(`EmptyFavoritesContainer should render correctly`, () => {
-  const {getByText} = render(
+  const {container} = render(
       <EmptyFavoritesContainer />
   );
 
-  const headerElement = getByText(`Nothing yet saved.`);
-  const paragraphElement = getByText(
-      `Save properties to narrow down search or plan your future trips.`
-  );
-
-  expect(headerElement).toBeInTheDocument();
-  expect(paragraphElement).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
 });
