ac8549a3674baba212c74c586df50c216d9aca18
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-29 12:46:11 +0200

    Delete sort-type prop

--- src/common/prop-types/sort-type.prop.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-import {SortType} from '../const';
-
-export const sortTypeProp = PropTypes.oneOf(
-    [SortType.POPULAR, SortType.PRICE_HIGHT_TO_LOW, SortType.PRICE_LOW_TO_HIGHT, SortType.TOP_RATED]
-);
