98bd21bcd586a063f1b7154ec1030cfddae55dcd
ElizabethSh <liza.shoshkina@gmail.com>
2021-03-29 12:43:57 +0200

    Refactoring

--- src/common/const.js
+++ src/common/const.js
@@ -1,3 +1,12 @@
+export const CITIES = [
+  `Paris`,
+  `Cologne`,
+  `Brussels`,
+  `Amsterdam`,
+  `Hamburg`,
+  `Dusseldorf`
+];
+
 export const CardName = {
   CITIES: `cities`,
   FAVORITES: `favorites`,
@@ -15,15 +24,6 @@ export const ButtonName = {
   FAVORITE: `favorite`,
   NEAR_PLACE: `near-place`
 };
-
-export const CITIES = [
-  `Paris`,
-  `Cologne`,
-  `Brussels`,
-  `Amsterdam`,
-  `Hamburg`,
-  `Dusseldorf`
-];
 
 export const SortType = {
   POPULAR: `popular`,
