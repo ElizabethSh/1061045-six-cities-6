48e73eb6dec21484ee5d33bd9826f3fa9fdf52fe
ElizabethSh <liza.shoshkina@gmail.com>
2021-04-02 15:09:10 +0200

    Add the validateEmail function

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,5 +1,10 @@
 const MAX_STARS_AMOUNT = 5;
 
+export const validateEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
 export const getCitiesList = (offers) => {
   const cities = [];
   offers.forEach((place) => cities.push(place.city.name));
