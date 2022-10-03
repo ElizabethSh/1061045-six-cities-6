9c05017dc5de40eee8e2b6d3f94aa15a87d23797
Lisa <liza.shoshkina@gmail.com>
2022-10-03 12:16:54 +0200

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
+    static: path.resolve(__dirname, 'public'),
     open: true,
     port: 1337,
     historyApiFallback: true
